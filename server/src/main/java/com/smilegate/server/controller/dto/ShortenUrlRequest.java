0d8b5a9ba43b8f0b60c8f0608364b487237a756d
hello-jiwon <jiwon.developer@gmail.com>
2021-11-05 11:54:22 +0900

    fix: validation 추가

--- server/src/main/java/com/smilegate/server/controller/dto/ShortenUrlRequest.java
+++ server/src/main/java/com/smilegate/server/controller/dto/ShortenUrlRequest.java
@@ -4,11 +4,16 @@ import com.smilegate.server.domain.Url;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
 
 @NoArgsConstructor
 @Getter
 public class ShortenUrlRequest {
 
+
+    @URL
+    @Length(min=1, max=1000)
     private String originUrl;
 
     @Builder
--- server/src/main/java/com/smilegate/server/exception/GlobalExceptionHandler.java
+++ server/src/main/java/com/smilegate/server/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.smilegate.server.exception;
 
 import com.smilegate.server.global.common.FailResponse;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -11,9 +12,18 @@ public class GlobalExceptionHandler {
 
     @ResponseStatus(value = HttpStatus.BAD_REQUEST)
     @ExceptionHandler(value = IllegalArgumentException.class)
-    public FailResponse handleUnAutorizedException(IllegalArgumentException e) {
+    public FailResponse handleIllegalArgumentException(IllegalArgumentException e) {
         return FailResponse.builder()
                 .errorMessage(e.getMessage())
+                .build();
+    }
+
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    @ExceptionHandler({MethodArgumentNotValidException.class})
+    public FailResponse handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        String errorMessage = e.getBindingResult().getAllErrors().get(0).getDefaultMessage();
+        return FailResponse.builder()
+                .errorMessage(errorMessage)
                 .build();
     }
 
